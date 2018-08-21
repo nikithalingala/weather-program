84fd9652eae49e2b47c28b0924e7c48393e47552
nikithalingala <nikithalingala@gmail.com>
2018-08-21 19:32:09 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Xaml;
+
+
+namespace world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            weather weather = new weather();
+            float tempkdp = weather.GetCurrentTemperature("kadapa", "in");
+            Console.WriteLine("temperature :  " + tempkdp);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ weather.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Net.Http;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Xaml;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+
+namespace world
+{
+    class weather
+    {
+
+        public float GetCurrentTemperature(string city, string countrycode)
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + countrycode + "&APPID=6eb1b56a44171680a7ac1497d62dedb6";
+            HttpClient httpClient = new HttpClient();
+            Task<string> taskAsString = httpClient.GetStringAsync(url);
+            string getUrlResult = taskAsString.Result;
+            exampleModel s = new JavaScriptSerializer().Deserialize<exampleModel>(getUrlResult);
+            return s.main.temp;
+        }
+    }
+    class exampleModel
+    {
+        public mainModel main { get; set; }
+    }
+    class mainModel
+    {
+        public float temp { get; set; }
+    }
+
+
+}
+
+
+
