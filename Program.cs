e22b5d6b9a3c9bfe74492013ebd3838d06750d56
nikithalingala <nikithalingala@gmail.com>
2018-08-29 00:46:08 +0530

    fourth

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ using Newtonsoft.Json.Converters;
 using System.Xaml;
 using System.IO;
 using System.Runtime.Serialization.Json;
-
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace world
 {
@@ -22,19 +23,41 @@ namespace world
         static void Main(string[] args)
         {
             //REAd configuration file into string
-            string filecontent = ReadConfigurationFile("cities.json");
+            string filecontent = ReadConfigurationFile("E:/c#/weatherProgram/world/cities.json");
             //parse filecontent
 
             CityConfig[] cityConfigArray = new JavaScriptSerializer().Deserialize<CityConfig[]>(filecontent);
             weather weather = new weather();
+            
+
             foreach (CityConfig cityConfig in cityConfigArray)
             {
                 Console.WriteLine(cityConfig.city);
               
                 float tempkdp = weather.GetCurrentTemperature(cityConfig.city, cityConfig.countrycode);
                 Console.WriteLine("temperature :  " + tempkdp);
+                //body
+                // HttpClient post
+                string url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityConfig.city + "," + cityConfig.countrycode + "&APPID=6eb1b56a44171680a7ac1497d62dedb6";
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                StringContent content = new StringContent(filecontent);
+               
+                content.Headers.Clear();
+                content.Headers.Add("Content-Type", "application/json");
+
+                HttpResponseMessage response = client.PostAsync("http://api.openweathermap.org/data/2.5/weather?q=" + cityConfig.city + "," + cityConfig.countrycode + "&APPID=6eb1b56a44171680a7ac1497d62dedb6", content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    DataContractJsonSerializer json = new DataContractJsonSerializer(typeof(CityConfig));
+                    
+                    string responseBody = response.Content.ReadAsStringAsync().Result;
+                    CityConfig Response = (CityConfig)json.ReadObject(response.Content.ReadAsStreamAsync().Result);
+                   Console.WriteLine(response);
+                }
             }
-           
+
             Console.ReadKey();
 
 
@@ -43,7 +66,7 @@ namespace world
         {
           
             //TODO remove hardcoding of content 
-          string JSONstring = File.ReadAllText("E:/c#/weatherProgram/world/cities.json");
+          string JSONstring = File.ReadAllText(configFileName);
 
                 
            return JSONstring ;
